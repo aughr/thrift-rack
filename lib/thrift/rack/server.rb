19b343a599a93f997e3b4d90bd9a9a9b7654b85e
Andrew Bloomgarden <stalkingtiger@gmail.com>
2013-03-16 16:56:37 -0700

    Extract Server to its own file.

---
+++ lib/thrift/rack/server.rb
@@ -0,0 +1,37 @@
+require 'thrift'
+require 'rack/request'
+require 'rack/response'
+
+require 'thrift/rack/processor'
+
+module Thrift
+  module Rack
+    class Server
+      THRIFT_CONTENT_TYPE = "application/x-thrift".freeze
+
+      def initialize(processor, options={})
+        @processor = processor
+        @processor.extend Thrift::Rack::Processor
+        @path = options[:path] || "/"
+        @protocol_factory = options[:protocol_factory] || BinaryProtocolFactory.new
+      end
+
+      def call(env)
+        request = ::Rack::Request.new env
+
+        if request.post? && request.path == @path
+          response = ::Rack::Response.new
+          response["Content-Type"] = THRIFT_CONTENT_TYPE
+
+          transport = IOStreamTransport.new request.body, response
+          protocol = @protocol_factory.get_protocol transport
+          response.status = @processor.process protocol, protocol
+          response
+        else
+          # empty 404
+          [404, {}, []]
+        end
+      end
+    end
+  end
+end
